8759fa13a0472f569d5cfe8610834f88794dfca0
say <sshee0123@naver.com>
2025-10-16 16:33:00 +0900

    Math, Random 클래스

---
+++ src/lang/system/SystemMain.java
@@ -0,0 +1,35 @@
+package lang.system;
+
+import java.util.Arrays;
+
+public class SystemMain {
+    public static void main(String[] args) {
+        // 현재 시간(밀리초)를 가져온다.
+        long currentTimeMills = System.currentTimeMillis();
+        System.out.println("currentTimeMills = " + currentTimeMills);
+
+        // 현재 시간(나노초)를 가져온다.
+        long currentTimeNano = System.nanoTime();
+        System.out.println("currentTimeNano = " + currentTimeNano);
+
+        // 환경 변수를 읽는다.
+        System.out.println("getenv = " + System.getenv());
+
+        // 시스템 속성을 읽는다.
+        System.out.println("properties = " + System.getProperties());
+        System.out.println("Java version: " + System.getProperty("java.version"));
+
+        // 배열을 고속으로 복사한다.
+        char[] originalArr = {'h','e','l','l','o'};
+        char[] copiedArr = new char[5];
+        System.arraycopy(originalArr, 0, copiedArr, 0, originalArr.length);
+
+        // 배열 출력
+        System.out.println("copiedArr = " + copiedArr);
+        System.out.println("copiedArr = " + Arrays.toString(copiedArr));
+
+        // 프로그램 종료
+        System.exit(0);
+
+    }
+}
