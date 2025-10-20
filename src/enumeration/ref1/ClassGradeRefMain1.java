c8abb760d36925ea1cca52d4d868546f1a1d0193
say <sshee0123@naver.com>
2025-10-20 15:45:57 +0900

    Enum 리팩토링

---
+++ src/enumeration/ref1/ClassGradeRefMain1.java
@@ -0,0 +1,16 @@
+package enumeration.ref1;
+
+
+public class ClassGradeRefMain1 {
+    public static void main(String[] args) {
+        int price = 10000;
+        DiscountService discountService = new DiscountService();
+        int basic = discountService.discount(ClassGrade.BASIC, price);
+        int gold = discountService.discount(ClassGrade.GOLD, price);
+        int diamond = discountService.discount(ClassGrade.DIAMOND, price);
+
+        System.out.println("basic 등급의 할인 금액= " + basic);
+        System.out.println("gold 등급의 할인 금액= " + gold);
+        System.out.println("diamond 등급의 할인 금액= " + diamond);
+    }
+}
---
+++ src/enumeration/ref3/EnumRefMain3_1.java
@@ -0,0 +1,15 @@
+package enumeration.ref3;
+
+public class EnumRefMain3_1 {
+    public static void main(String[] args) {
+        int price = 10000;
+        DiscountService discountService = new DiscountService();
+        int basic = discountService.discount(Grade.BASIC, price);
+        int gold = discountService.discount(Grade.GOLD, price);
+        int diamond = discountService.discount(Grade.DIAMOND, price);
+
+        System.out.println("basic 등급의 할인 금액= " + basic);
+        System.out.println("gold 등급의 할인 금액= " + gold);
+        System.out.println("diamond 등급의 할인 금액= " + diamond);
+    }
+}
